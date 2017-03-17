3b85d4ff5e1b055a2d4ed408798b3ba30f08a5ba
twpl <tea@test.jp>
2017-03-17 18:53:55 +0900

    fix

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var lg      = require('./lib/log.js');
 
 // checking _config.yml
 if(!hexo.config.randomEmbed || !hexo.config.randomEmbed.list){
-	lg.log("error", "Please set the 'randomEmbed.lists' option." , "_config.yml");
-	return;
+  lg.log("error", "Please set the 'randomEmbed.lists' option." , "_config.yml");
+  return;
 }
 
 // loading lists
@@ -19,36 +19,62 @@ var listJson = JSON.parse(listData);
 
 // orverride config.randomEmbed
 hexo.config.randomEmbed = assign( {} ,
-	hexo.config.randomEmbed , {
-		"listData" : listJson
-	}
+  hexo.config.randomEmbed , {
+    "listData" : listJson
+  }
 );
+
 
 
 // hexo.extend.tag.register('randomEmbed', require('./lib/helper'));
 hexo.extend.tag.register('randomEmbed', function(args , content) {
-    
+
     var returnHTML = "";
-    
+
     var matchList  = [];
-	var listData   = hexo.config.randomEmbed.listData;
-	var listCount  = Number(args[0]);
-	var instanceID = String(args[1]);
-	var tags       = [];
-	var i          = 0;
-    
-    if(args.length > 2){
-      for(i = 2; i < args.length; i++){
-        tags.push(args[i]);
+    var listData   = hexo.config.randomEmbed.listData;
+    var listCount  = Number(args[0]);
+    var instanceID = String(args[1]);
+    var tags       = [];
+    var classes    = "";
+    var i          = 0;
+    var FORCENUM   = 9999;
+
+
+    function shuffle(array) {
+      var m = array.length, t, i;
+
+      // While there remain elements to shuffle…
+      while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
       }
+      return array;
     }
     
+
+    if(args.length > 2){
+      for(i = 2; i < args.length; i++){
+        if(args[i].match(/^(class\-|class\:)/)){
+          classes += (classes != "" ? " " : "") + args[i].replace(/^(class\-|class\:)/,"");
+        }else{
+          tags.push(args[i]);
+        }
+      }
+    }
+    // 
     // checking args
     if( !listData || listData == "" || listData.length == 0 || !listCount || (tags.length == 0 && (!instanceID || instanceID == "")) )return returnHTML;
-    
+
     for( i = 0; i < listData.length; i++){
       if( listData[i].instanceID == instanceID ){
-        matchList.push({ "mc" : 9999 , "htmlData" : listData[i].htmlData });
+        matchList.push({ "mc" : FORCENUM , "htmlData" : listData[i].htmlData });
       }else{
         var matchCnt = 0;
         for( var j = 0; j < tags.length; j++){
@@ -62,16 +88,22 @@ hexo.extend.tag.register('randomEmbed', function(args , content) {
 
     // sort by matchCnt
     matchList.sort(function (a,b){
-    	if(b.mc == a.mc){
-    		return (Math.random()*100 + 1) > 50 ? 1 : -1;
-    	}else{
-    		return b.mc - a.mc;
-    	}
+      if(b.mc == a.mc){
+        return (Math.random()*100 + 1) > 50 ? 1 : -1;
+      }else{
+        return b.mc - a.mc;
+      }
     });
-    
-    for( i = 0; i < listCount && i < matchList.length; i++){
-    	returnHTML += matchList[i].htmlData;
+
+    if(matchList.length > 0 && matchList[0].mc != FORCENUM ){
+      matchList = shuffle(matchList);
     }
 
-  	return returnHTML;
+    for( i = 0; i < listCount && i < matchList.length; i++){
+      returnHTML += matchList[i].htmlData;
+    }
+
+    if(classes != "")returnHTML = '<div class="' + classes + '">' + returnHTML + '</div>';
+
+    return returnHTML;
 });
